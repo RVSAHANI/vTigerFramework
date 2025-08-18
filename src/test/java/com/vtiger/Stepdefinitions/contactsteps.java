9388cba9b936443a06b72ceb285913105de76828
gunjanbendale21 <gunjanbendale21@gmail.com>
2025-08-18 21:57:10 +0530

    remaining changes for jenkins integration with git

---
+++ src/test/java/com/vtiger/Stepdefinitions/contactsteps.java
@@ -0,0 +1,22 @@
+package com.vtiger.Stepdefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class contactsteps {
+    @When("user click on new contact link")
+    public void user_click_on_new_contact_link() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user enters the Last Name and click on save button")
+    public void user_enters_the_last_name_and_click_on_save_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Contact should be created successfully")
+    public void contact_should_be_created_successfully() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
